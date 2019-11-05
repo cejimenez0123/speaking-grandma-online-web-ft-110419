736ab4e6a5b98c655a4230ad0c97b0814a9d9483
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-05 14:52:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,16 +13,16 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN";
+    return "I LOVE YOU TOO PUMPKIN!";
   elsif phrase == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY";
+    return "HUH?! SPEAK UP, SONNY!";
   elsif phrase == "Hi!"
-    return "HUH?! SPEAK UP, SONNY";
+    return "HUH?! SPEAK UP, SONNY!";
   elsif phrase == "WHAT DID YOU EAT TODAY"
     return "NO, NOT SINCE 1938!";
   elsif phrase == "WHAT?"
     return"NO, NOT SINCE 1938!";
-  else return "HUH?! SPEAK UP, SONNY";
+  else return "HUH?! SPEAK UP, SONNY!";
     binding.pry
         speak_to_grandma(phrase)
 end
